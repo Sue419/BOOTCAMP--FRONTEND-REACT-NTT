05831ddedac0944e71ad83f59f9940ebd7fcb393
Sue419 <suzan419@gmail.com>
2024-11-17 19:20:17 -0500

    feat: add component productCard

---
+++ miniMarket/src/components/productCard.js
@@ -0,0 +1,97 @@
+export const createProductCard = (product) => {
+    const productCard = document.createElement('article');
+    productCard.classList.add('product');
+
+    //Imagen
+    const contentProductImage = createImageContainer(product);
+  
+    //Información
+    const cardInfo = createCardInfo(product);
+  
+    //Botón
+    const addToCartButton = createAddToCartButton();
+  
+    //Orden
+    productCard.appendChild(contentProductImage);
+    productCard.appendChild(cardInfo);
+    productCard.appendChild(addToCartButton);
+  
+    const productContainer = document.querySelector('.product-container');
+    productContainer.appendChild(productCard);
+
+    return productCard;
+};
+
+// Imagen del producto
+const createImageContainer = (product) => {
+    const contentProductImage = document.createElement('div');
+    contentProductImage.classList.add('product-image');
+    
+    const productImage = document.createElement('img');
+    productImage.src = product.thumbnail;
+    productImage.alt = product.title;
+    
+    contentProductImage.appendChild(productImage);
+    return contentProductImage;
+};
+
+// Iformación producto (título, descripción, categoría, precio)
+const createCardInfo = (product) => {
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('card-info');
+
+    const productBrand = document.createElement('h4');
+    productBrand.classList.add('product-brand');
+    productBrand.textContent = product.brand;
+  
+    const productTitle = document.createElement('h2');
+    productTitle.classList.add('product-title');
+    productTitle.textContent = product.title;
+  
+    const productDescription = document.createElement('p');
+    productDescription.classList.add('product-description');
+    productDescription.textContent = product.description;
+  
+    const productCategory = document.createElement('p');
+    productCategory.classList.add('product-category');
+    productCategory.textContent = ``;
+    
+    const tagCategory = document.createElement('span');
+    tagCategory.textContent = `${product.category}`;
+  
+    const productPrice = document.createElement('p');
+    productPrice.classList.add('product-price');
+    productPrice.textContent = `Price:`;
+
+    const productAmountPrice = document.createElement('span');
+    productAmountPrice.classList.add('product-price');
+    productAmountPrice.textContent = `S/ ${product.price}`;
+    
+    cardInfo.appendChild(productBrand)
+    cardInfo.appendChild(productTitle);
+    cardInfo.appendChild(productCategory);
+    cardInfo.appendChild(productDescription);
+
+    productCategory.appendChild(tagCategory);
+    cardInfo.appendChild(productPrice);
+    productPrice.appendChild(productAmountPrice);
+  
+    return cardInfo;
+};
+
+//Botón de añadir al carrito
+const createAddToCartButton = () => {
+    const addToCartButton = document.createElement('button');
+    addToCartButton.classList.add('add-to-cart-btn');
+    addToCartButton.textContent = `Add to cart`;
+
+    const cartIcon = document.createElement('div');
+    cartIcon.classList.add('cart-icon_btn');
+    const cartImage = document.createElement('img');
+    cartImage.src = './src/assets/icons/cart-shopping_white.svg';
+    cartImage.alt = 'Cart Icon';
+    cartIcon.appendChild(cartImage);
+
+    addToCartButton.appendChild(cartIcon);
+    return addToCartButton;
+};
