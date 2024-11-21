b7d46ae726816181168beb699df43c69cf8216a2
Sue419 <suzan419@gmail.com>
2024-11-21 15:26:03 -0500

    feat: update searchProducts function to filter products using includes

--- miniMarket/src/js/search.js
+++ miniMarket/src/js/search.js
@@ -2,9 +2,11 @@ import { updateProductCards } from "./updateProductCard";
 
 export function searchProducts(event, products) {
     const query = event.target.value.toLowerCase();
+
     const filteredProducts = products.filter(product =>
-        product.title.toLowerCase().includes(query) || product.description.toLowerCase().includes(query) || 
-        product.category.toLowerCase().includes(query)
+        [product.title, product.description, product.category, product.brand]
+            .some(property => property && property.toLowerCase().includes(query))
     );
+
     updateProductCards(filteredProducts);
 }
