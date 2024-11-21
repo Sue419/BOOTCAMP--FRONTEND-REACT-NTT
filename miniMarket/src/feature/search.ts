783758a79084f44ada7ed3e69ffed1dfe2b2d737
bootcamp <bootcampntt2024@gmail.com>
2024-11-21 10:51:46 -0500

    check ts

--- miniMarket/src/feature/search.ts
+++ miniMarket/src/feature/search.ts
@@ -3,6 +3,7 @@ import { updateProductCards } from "./updateProductCard";
 
 export function searchProducts(event: InputEvent, products: Product[]) {
         const query = (event.target as HTMLInputElement).value.toLowerCase();
+        // usar include
         const filteredProducts = products.filter(product =>
         product.title.toLowerCase().includes(query) || product.description.toLowerCase().includes(query) || 
         product.category.toLowerCase().includes(query)
