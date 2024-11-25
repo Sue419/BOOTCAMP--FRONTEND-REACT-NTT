c0b03ad543823f64c7183039000f349b6534fa99
Sue419 <suzan419@gmail.com>
2024-11-24 21:06:39 -0500

    feat: add hooks, useLocalStorage and useModal

---
+++ miniMarket/src/hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [storedValue, key]);
+
+  return {
+    storedValue,
+    setStoredValue,
+  };
+};
