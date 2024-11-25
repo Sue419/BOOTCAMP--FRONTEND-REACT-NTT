4f688246a9821424160974f5d37b2a48da2e1d6d
Sue419 <suzan419@gmail.com>
2024-11-24 21:09:45 -0500

    feat: update cartContext, useReducer and useLocalStorage

--- miniMarket/src/context/cartContext.tsx
+++ miniMarket/src/context/cartContext.tsx
@@ -1,32 +1,62 @@
-import { createContext, useState, ReactNode } from "react";
-import { Product } from "../domain/product";
+import { useEffect, createContext, useReducer, ReactNode } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage"; // Asegúrate de importar correctamente
+import { Product, CartProduct } from "../domain/product";
+import { cartReducer } from "./cartReducer";
+
 
 interface CartContextProps {
-  cart: Product[];
+  cart: CartProduct[];
   cartCount: number;
   addToCart: (product: Product) => void;
-  getProductById: (productId: string) => Product | undefined;
+  removeFromCart: (productId: number) => void;
+  incrementQuantity: (productId: number) => void;
+  decrementQuantity: (productId: number) => void;
+  getProductById: (productId: number) => CartProduct | undefined;
 }
 
 const CartContext = createContext<CartContextProps | undefined>(undefined);
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [cart, setCart] = useState<Product[]>([]);
+  const { setStoredValue, storedValue } = useLocalStorage<CartProduct[]>("cart", []);
+
+  // storedValue estado inicial
+  const [state, dispatch] = useReducer(cartReducer, {
+    cart: storedValue || [],
+  });
+
+  useEffect(() => {
+    setStoredValue(state.cart);
+  }, [state.cart, setStoredValue]);
 
   const addToCart = (product: Product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    dispatch({ type: "ADD_TO_CART", product: { ...product, quantity: 1 } });
   };
 
-  const getProductById = (productId: string): Product | undefined => {
-    return cart.find((product) => product.id.toString() === productId);
+  const removeFromCart = (productId: number) => {
+    dispatch({ type: "REMOVE_FROM_CART", productId });
+  };
+
+  const incrementQuantity = (productId: number) => {
+    dispatch({ type: "INCREMENT", productId });
+  };
+
+  const decrementQuantity = (productId: number) => {
+    dispatch({ type: "DECREMENT", productId });
+  };
+
+  const getProductById = (productId: number): CartProduct | undefined => {
+    return state.cart.find((product) => product.id === productId);
   };
 
   return (
     <CartContext.Provider
       value={{
-        cart,
-        cartCount: cart.length,
+        cart: state.cart,
+        cartCount: state.cart.reduce((count, product) => count + product.quantity, 0),
         addToCart,
+        removeFromCart,
+        incrementQuantity,
+        decrementQuantity,
         getProductById,
       }}
     >
