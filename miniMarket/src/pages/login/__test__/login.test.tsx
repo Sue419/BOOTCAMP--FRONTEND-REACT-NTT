2849a419fccb0811b2928ef478225822b3ac3c3e
Sue419 <suzan419@gmail.com>
2024-12-02 19:15:36 -0500

    feat: add test login

--- miniMarket/src/pages/login/__test__/login.test.tsx
+++ miniMarket/src/pages/login/__test__/login.test.tsx
@@ -2,15 +2,18 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import { AppRoutes } from "@/constants/routes";
 import LoginPage from "../login";
+import { AuthProvider } from "@/context/auth/authContext";
 
 describe("Login", () => {
   it("renders the LoginPage component at the correct route", () => {
     render(
-      <MemoryRouter initialEntries={[AppRoutes.Login]}>
-        <Routes>
-          <Route path={AppRoutes.Login} element={<LoginPage />} />
-        </Routes>
-      </MemoryRouter>
+      <AuthProvider>
+        <MemoryRouter initialEntries={[AppRoutes.Login]}>
+          <Routes>
+            <Route path={AppRoutes.Login} element={<LoginPage />} />
+          </Routes>
+        </MemoryRouter>
+      </AuthProvider>
     );
 
     expect(screen.getByRole("heading", { name: /login/i })).toBeInTheDocument();
